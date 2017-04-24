5ce15119d2afa164fb975a41fa04239bc91f9360
Atwynss <n.chinari@gmail.com>
2017-04-24 11:51:09 +0530

    changes in Tololbar

--- app/src/main/java/com/example/admin/abc/Config.java
+++ app/src/main/java/com/example/admin/abc/Config.java
@@ -6,23 +6,23 @@ package com.example.admin.abc;
 
 public class Config {
 
-public final static String productsUrlAddress = "http://192.168.0.5/abc/getProducts.php";
+public final static String productsUrlAddress = "http://192.168.0.3/abc/getProducts.php";
 
-public final static String productTypesUrlAddress = "http://192.168.0.5/abc/getProductTypes.php?ProductId=";
+public final static String productTypesUrlAddress = "http://192.168.0.3/abc/getProductTypes.php?ProductId=";
 
-public final static String productSizesUrlAddress = "http://192.168.0.5/abc/getProductSizes.php?ProductId=";
+public final static String productSizesUrlAddress = "http://192.168.0.3/abc/getProductSizes.php?ProductId=";
 
-public final static String productTypeSubTypesUrlAddress = "http://192.168.0.5/abc/getProductTypeSubTypes.php?ProductTypeId=";
+public final static String productTypeSubTypesUrlAddress = "http://192.168.0.3/abc/getProductTypeSubTypes.php?ProductTypeId=";
 
-public final static String productTypeSizesUrlAddress = "http://192.168.0.5/abc/getProductTypeSizes.php?";
+public final static String productTypeSizesUrlAddress = "http://192.168.0.3/abc/getProductTypeSizes.php?";
 
-public final static String productTypeSubTypeImgUrlAddress = "http://192.168.0.5/abc/getProductTypeSubTypeImages.php?ProductSubTypeId=";
+public final static String productTypeSubTypeImgUrlAddress = "http://192.168.0.3/abc/getProductTypeSubTypeImages.php?ProductSubTypeId=";
 
-public final static String productTypeImgUrlAddress = "http://192.168.0.5/abc/getProductTypeImages.php?";
+public final static String productTypeImgUrlAddress = "http://192.168.0.3/abc/getProductTypeImages.php?";
 
-public final static String productTypeSizeImgUrlAddress ="http://192.168.0.5/abc/getProductTypeSizeImages.php?";
+public final static String productTypeSizeImgUrlAddress ="http://192.168.0.3/abc/getProductTypeSizeImages.php?";
 
-public final static String productSizeImgUrlAddress = "http://192.168.0.5/abc/getProductSizeImages.php?";
+public final static String productSizeImgUrlAddress = "http://192.168.0.3/abc/getProductSizeImages.php?";
 
 public final static String PRODUCTID_PARAM ="ProductId";
 public final static String PRODUCTTYPEID_PARAM ="ProductTypeId";
