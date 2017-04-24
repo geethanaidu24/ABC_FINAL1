01da98314b270edf8be0e3571a519aa05d8b65c6
Atwynss <n.chinari@gmail.com>
2017-04-24 17:08:08 +0530

    changes in Tololbar

---
+++ app/src/main/java/com/example/admin/abc/ProductSubTypeSingleViewImageFull.java
@@ -0,0 +1,70 @@
+package com.example.admin.abc;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+import static android.R.attr.name;
+import static com.example.admin.abc.R.id.image;
+
+/**
+ * Created by Admin on 4/24/2017.
+ */
+
+public  class ProductSubTypeSingleViewImageFull  extends AppCompatActivity {
+    ImageView im;
+    Context c;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_size_single_view_full_image);
+
+
+
+        im = (ImageView) findViewById(R.id.fullimage);
+
+        Intent i = this.getIntent(); // get Intent which we set from Previous Activity
+        final int pid = i.getExtras().getInt("PRODUCTID_KEY");
+        final String pname = i.getExtras().getString("PRODUCTNAME_KEY");
+        final int ptid = i.getExtras().getInt("PRODUCTTYPEID_KEY");
+        final String ptname = i.getExtras().getString("PRODUCTTYPENAME_KEY");
+        final int pstid = i.getExtras().getInt("PRODUCTSUBTYPEID_KEY");
+        final String name = i.getExtras().getString("NAME_KEY");
+        final String image = i.getExtras().getString("IMAGE_KEY");
+        final String brand = i.getExtras().getString("BRAND_KEY");
+        final String color = i.getExtras().getString("COLOR_KEY");
+        PicassoClient.downloadImage(c,image,im);
+
+        // Picasso.with(c).load("http://192.168.0.3/abc/getProductSizeImages.php?").into(im);
+        Toolbar actionbar = (Toolbar) findViewById(R.id.toolbar);
+        if (null != actionbar) {
+            actionbar.setNavigationIcon(R.mipmap.backbutton);
+
+            //  actionbar.setTitle(R.string.title_activity_settings);
+            actionbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent in = new Intent(ProductSubTypeSingleViewImageFull.this, ProductSubTypeSingleViewActivity.class);
+
+                    in.putExtra("IMAGE_KEY",image);
+                    in.putExtra("PRODUCTID_KEY",pid);
+                    in.putExtra("PRODUCTNAME_KEY",pname);
+                    in.putExtra("PRODUCTTYPEID_KEY",ptid);
+                    in.putExtra("PRODUCTTYPENAME_KEY",ptname);
+                    in.putExtra("PRODUCTSUBTYPEID_KEY",pstid);
+                    in.putExtra("NAME_KEY",name);
+                    in.putExtra("BRAND_KEY",brand);
+                    in.putExtra("COLOR_KEY",color);
+                    startActivity(in);
+                }
+            });
+
+        }
+    }
+}
