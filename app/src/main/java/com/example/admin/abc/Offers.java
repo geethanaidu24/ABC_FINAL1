3e0e54c11b0059ccaddeaa893fbbda18eed94101
Atwynss <n.chinari@gmail.com>
2017-04-24 15:14:40 +0530

    changes in Tololbar

--- app/src/main/java/com/example/admin/abc/Offers.java
+++ app/src/main/java/com/example/admin/abc/Offers.java
@@ -3,24 +3,30 @@ package com.example.admin.abc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 
 public class Offers extends AppCompatActivity {
-ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offers);
-        imageView=(ImageView)findViewById(R.id.back);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent in=new Intent(Offers.this,Main2Activity.class);
-                startActivity(in);
-            }
-        });
+        Toolbar actionbar = (Toolbar) findViewById(R.id.toolbar);
+        if (null != actionbar) {
+            actionbar.setNavigationIcon(R.mipmap.backbutton);
+
+            //  actionbar.setTitle(R.string.title_activity_settings);
+            actionbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent in = new Intent(Offers.this, Main2Activity.class);
+                    startActivity(in);
+                }
+            });
+        }
     }
 }
