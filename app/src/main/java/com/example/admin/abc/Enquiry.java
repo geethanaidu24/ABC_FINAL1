c44da9a3c8d7a0792344294a8268863a96dd59fc
SYS3 <khushbunahar@gmail.com>
2017-05-11 16:48:55 +0530

    Changes add pages

--- app/src/main/java/com/example/admin/abc/Enquiry.java
+++ app/src/main/java/com/example/admin/abc/Enquiry.java
@@ -5,13 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import cz.msebera.android.httpclient.util.TextUtils;
 
 public class Enquiry extends AppCompatActivity {
 ImageView im;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-      //  getSupportActionBar().hide();
+        //  getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enquiry);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -23,23 +31,104 @@ ImageView im;
             toolbar.setNavigationOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent in=new Intent(Enquiry.this,Main2Activity.class);
+                    Intent in = new Intent(Enquiry.this, Main2Activity.class);
                     finish();
-                  //  startActivity(in);
+                    //  startActivity(in);
                 }
             });
+            final EditText your_name = (EditText) findViewById(R.id.editText);
+            final EditText your_email = (EditText) findViewById(R.id.editText3);
+            final EditText your_subject = (EditText) findViewById(R.id.editText4);
+            final EditText your_message = (EditText) findViewById(R.id.editText5);
 
-            // Inflate a menu to be displayed in the toolbar
-           // actionbar.inflateMenu(R.menu.actions);
+
+            Button email = (Button) findViewById(R.id.button7);
+            email.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    String name = your_name.getText().toString();
+                    String email = your_email.getText().toString();
+                    String subject = your_subject.getText().toString();
+                    String message = your_message.getText().toString();
+                    if (TextUtils.isEmpty(name)) {
+                        your_name.setError("Enter Your Name");
+                        your_name.requestFocus();
+                        return;
+                    }
+
+                    Boolean onError = false;
+                    if (!isValidEmail(email)) {
+                        onError = true;
+                        your_email.setError("Invalid Email");
+                        return;
+                    }
+
+                    if (TextUtils.isEmpty(subject)) {
+                        your_subject.setError("Enter Your Contact Number");
+                        your_subject.requestFocus();
+                        return;
+                    }
+
+                    if (TextUtils.isEmpty(message)) {
+                        your_message.setError("Enter Your Message");
+                        your_message.requestFocus();
+                        return;
+                    }
+
+                    Intent sendEmail = new Intent(Intent.ACTION_SEND);
+
+            /* Fill it with Data */
+
+                    sendEmail.putExtra(Intent.EXTRA_EMAIL, new String[]{"khushbunahar@gmail.com"});
+                    sendEmail.putExtra(Intent.EXTRA_SUBJECT, subject);
+                    sendEmail.putExtra(Intent.EXTRA_TEXT,
+                            "name:" + name + '\n' + "Email ID:" + email + '\n' + "Message:" + '\n' + message);
+                    sendEmail.setType("message/rfc822");
+            /* Send it off to the Activity-Chooser */
+
+                    try {
+                        startActivity(Intent.createChooser(sendEmail, "Select email Client...."));
+                    } catch (android.content.ActivityNotFoundException ex) {
+                        Toast.makeText(Enquiry.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+        }
+    }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            //Get a Tracker (should auto-report)
+
+
         }
 
-       /* im=(ImageView)findViewById(R.id.back);
-        im.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent in=new Intent(Enquiry.this,Main2Activity.class);
-                startActivity(in);
-            }
-        });*/
+        @Override
+        protected void onStart() {
+            super.onStart();
+
+        }
+
+        @Override
+        protected void onStop() {
+            super.onStop();
+
+        }
+
+
+        // validating email id
+
+    private boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
+
+
 }
