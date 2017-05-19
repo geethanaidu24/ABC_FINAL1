aab815014d43931e585e9da3a6c7d72b901a23d4
geethanaidu <geethamca.naidu@gmail.com>
2017-05-19 13:13:29 +0530

    add and display contacts

--- app/src/main/java/com/example/admin/abc/AddContact.java
+++ app/src/main/java/com/example/admin/abc/AddContact.java
@@ -20,8 +20,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+
 import net.gotev.uploadservice.MultipartUploadRequest;
 import net.gotev.uploadservice.UploadNotificationConfig;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -32,8 +39,6 @@ import cz.msebera.android.httpclient.util.TextUtils;
 
 public class AddContact extends AppCompatActivity implements View.OnClickListener {
     private static final String UPLOAD_URL = Config.contactsCRUD;
-
-    private static final int STORAGE_PERMISSION_CODE = 123;
 
     private EditText branchname,address,city,contactnumber,email,workinghours;
 
@@ -74,8 +79,7 @@ public class AddContact extends AppCompatActivity implements View.OnClickListene
         contactnumber = (EditText) findViewById(R.id.editText12);
         email=(EditText) findViewById(R.id.editText14);
         workinghours=(EditText) findViewById(R.id.editText15);
-        btnUpload = (Button)findViewById(R.id.btnUpload);
-
+        btnUpload = (Button)findViewById(R.id.button2);
 
         btnUpload.setOnClickListener(this);
     }
@@ -93,8 +97,8 @@ public class AddContact extends AppCompatActivity implements View.OnClickListene
         String baddress = address.getText().toString();
         String bcontactno=contactnumber.getText().toString();
         String bcity = city.getText().toString();
-
         String bemail = email.getText().toString();
+        String bworkinghrs = workinghours.getText().toString();
 
         if (TextUtils.isEmpty(bname)) {
             branchname.setError("Fill All");
@@ -103,22 +107,36 @@ public class AddContact extends AppCompatActivity implements View.OnClickListene
         }
 
         Boolean onError = false;
-        if (!isValidEmail(bemail)) {
-            onError = true;
-            email.setError("Invalid Email");
-            return;
+        if(!TextUtils.isEmpty(bemail)){
+            if (!isValidEmail(bemail)) {
+                onError = true;
+                email.setError("Invalid Email");
+                return;
+            }
         }
-Boolean onErrorr=false;
-        if(!isValidPhone(bcontactno))
+
+        //Boolean onErrorr=false;
+        if(!TextUtils.isEmpty(bcontactno)) {
+            if (!isValidPhone(bcontactno)) {
+                onError = true;
+                email.setError("Invalid contact");
+                return;
+            }
+        }
         if (TextUtils.isEmpty(baddress)) {
-            contactnumber.setError("Enter Your Contact Number");
-            contactnumber.requestFocus();
+            address.setError("Enter Your Branch Address");
+            address.requestFocus();
             return;
         }
 
         if (TextUtils.isEmpty(bcity)) {
-            city.setError("Enter Your Message");
+            city.setError("Enter Your city");
             city.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(bworkinghrs)) {
+            workinghours.setError("Enter Your Timings");
+            workinghours.requestFocus();
             return;
         }
 
@@ -126,20 +144,11 @@ Boolean onErrorr=false;
             Toast.makeText(this, "Successfully Completed", Toast.LENGTH_SHORT).show();
             branchname.setText("");
             address.setText("");
-        city.setText("");
-        email.setText("");
-        contactnumber.setText("");
-        workinghours.setText("");
-
-           /* adapter.notifyDataSetChanged();
-            BackTask bt = new BackTask();
-            bt.execute();*/
+            city.setText("");
+            email.setText("");
+            contactnumber.setText("");
+            workinghours.setText("");
         }
-
-
-
-
-
     public void uploadMultipart() {
         String bname = branchname.getText().toString();
         String baddress = address.getText().toString();
@@ -151,22 +160,49 @@ Boolean onErrorr=false;
         String bworkinghours=workinghours.getText().toString();
 
         try {
-            String uploadId = UUID.randomUUID().toString();
+            AndroidNetworking.post(UPLOAD_URL)
+                    .addBodyParameter("action","save")
+                    .addBodyParameter("branch", bname)
+                    .addBodyParameter("address", baddress)
+                    .addBodyParameter("city", bcity)
+                    .addBodyParameter("email", bemail)
+                    .addBodyParameter("contact", bcontactno)
+                    .addBodyParameter("workinghrs", bworkinghours)
+                    .setTag("TAG_ADD")
+                    .build()
+                    .getAsJSONArray(new JSONArrayRequestListener() {
+                        @Override
+                        public void onResponse(JSONArray response) {
+                            if(response != null)
+                                try {
+                                    //SHOW RESPONSE FROM SERVER
+                                    String responseString = response.get(0).toString();
+                                    Toast.makeText(AddContact.this, "PHP SERVER RESPONSE : " + responseString, Toast.LENGTH_SHORT).show();
+                                    if (responseString.equalsIgnoreCase("Success")) {
+                                      /*  //CLEAR EDITXTS
+                                        txtwidth.setText("");
+                                        txtheight.setText("");
+                                        txtlength.setText("");*/
+                                        /*adapter.notifyDataSetChanged();
 
-            //Creating a multi part request
-            new MultipartUploadRequest(this, uploadId, UPLOAD_URL)
-                    .addParameter("action","save")
+                                       BackTask bt = new BackTask();
+                                        bt.execute();*/
 
-                    .addParameter("branch", bname) //Adding text parameter to the request
-                    .addParameter("address", baddress)
-                    .addParameter("city", bcity)
-                    .addParameter("email", bemail)
-                    .addParameter("contact", bcontactno)
-                    .addParameter("workinghrs", bworkinghours)
-
-                    .setNotificationConfig(new UploadNotificationConfig())
-                    .setMaxRetries(2)
-                    .startUpload(); //Starting the upload
+                                    }else
+                                    {
+                                        Toast.makeText(AddContact.this, "PHP WASN'T SUCCESSFUL. ", Toast.LENGTH_SHORT).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                    Toast.makeText(AddContact.this, "GOOD RESPONSE BUT JAVA CAN'T PARSE JSON IT RECEIVED : "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                        }
+                        //ERROR
+                        @Override
+                        public void onError(ANError anError) {
+                            Toast.makeText(AddContact.this, "UNSUCCESSFUL :  ERROR IS : "+anError.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
 
         } catch (Exception exc) {
             Toast.makeText(this, exc.getMessage(), Toast.LENGTH_SHORT).show();
@@ -174,61 +210,20 @@ Boolean onErrorr=false;
 
     }
 
-/*
-    public String getPath(Uri uri) {
-        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-        cursor.moveToFirst();
-        String document_id = cursor.getString(0);
-        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
-        cursor.close();
 
-        cursor = getContentResolver().query(
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
-        cursor.moveToFirst();
-        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
-        cursor.close();
-
-        return path;
-    }
-
-    private void requestStoragePermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
-            return;
-
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            //If the user has denied the permission previously your code will come to this block
-            //Here you can explain why you need this permission
-            //Explain here why you need this permission
-        }
-        //And finally ask for the permission
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
-    }
-
-    //This method will be called when the user will tap on allow or deny
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        //Checking the request code of our request
-        if (requestCode == STORAGE_PERMISSION_CODE) {
-
-            //If permission is granted
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                //Displaying a toast
-                Toast.makeText(this, "Permission granted now you can read the storage", Toast.LENGTH_LONG).show();
-            } else {
-                //Displaying another toast if permission is not granted
-                Toast.makeText(this, "Oops you just denied the permission", Toast.LENGTH_LONG).show();
-            }
-        }
-    }*/
     private boolean isValidEmail(String email) {
         String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
                 + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
+        if (matcher.matches())
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
     public static boolean isValidPhone(String phone)
     {
