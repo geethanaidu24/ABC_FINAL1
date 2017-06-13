28488538cb50b9edacd47fa7344072c1776a2ec2
Geetha <geethamca.naidu@gmail.com>
2017-06-13 17:13:52 +0530

    touch double

--- app/src/main/java/com/example/admin/abc/Products.java
+++ app/src/main/java/com/example/admin/abc/Products.java
@@ -18,6 +18,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -659,6 +660,20 @@ public class Products extends AppCompatActivity implements Serializable {
             }
             return 0;
         }
+    }
+    public static void disableTouchTheft(View view) {
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                view.getParent().requestDisallowInterceptTouchEvent(true);
+                switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+                    case MotionEvent.ACTION_UP:
+                        view.getParent().requestDisallowInterceptTouchEvent(false);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
 }
